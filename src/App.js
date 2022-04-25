997e643a7ba5f37a59f97942437e73510a4d80a7
Lompsolonmukka <102826760+Lompsolonmukka@users.noreply.github.com>
2022-04-25 23:46:20 +0300

    Alpha build 1.0
    Bare bones for inventory system. Search not working, Add is working.

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import "./App.css";
+import SearchBar from "./SearchBar";
+import AddItem from "./AddItem";
+import { useEffect, useState } from "react";
+import ItemsDisplay from "./ItemsDisplay";
+
+function App() {
+  const [filters, setFilters] = useState({});
+  const [data, setData] = useState({ items: [] });
+
+  useEffect(() => {
+    fetch("http://localhost:3000/items")
+      .then((response) => response.json())
+      .then((data) => setData({ items: data }));
+  }, []);
+
+  const updateFilters = (searchParams) => {
+    setFilters(searchParams);
+  };
+
+  const addItemToData = (item) => {
+    let items = data["items"];
+    
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(item)
+    };
+    fetch("http://localhost:3000/items", requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        items.push(data);
+        setData({ items: items });
+      }
+    );
+    
+  };
+
+  const filterData = (data) => {
+    const filteredData = [];
+
+    if (!filters.name) {
+      return data;
+    }
+
+    for (const item of data) {
+
+      if (filters.name !== "" && item.name !== filters.name) {
+        continue;
+      }
+      
+      if (filters.price !== 0 && item.price > filters.price) {
+        continue;
+      }
+      
+      if (filters.type !== "" && item.type !== filters.type) {
+        continue;
+      }
+      
+      if (filters.brand !== "" && item.brand !== filters.brand) {
+        continue;
+      }
+      filteredData.push(item);
+    }
+    return [filteredData];
+  };
+
+  return (
+    <div className="container">
+            <div className = "row mt-3">
+      <ItemsDisplay items={filterData(data["items"])} />
+      </div>
+
+      <div className = "row mt-3">
+        <SearchBar updateSearchParams={updateFilters} /> {/*For the childcomponent SearchBar to update the parent by callback */}
+      </div>
+
+      <div className = "row mt-3">
+        <AddItem addItem={addItemToData} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
